22dc670d7ba497af740ea26673e736bb9a74e8f0
Srabutdotcom <handoko75@gmail.com>
2023-12-15 11:13:48 +0000

    update

---
+++ whatis/whatis.js
@@ -0,0 +1,11 @@
+/**
+ * Returns the type of a value.
+ * @param {any} d - the value to check
+ * @returns {string|number|boolean|symbol|undefined|null|object|function} - the type of the value
+ */
+export function whatis(d) {
+   if (d === null) return 'null';
+   const t = typeof d;
+   if (t !== 'object') return t;
+   return d?.constructor?.name ?? 'Object';
+}
